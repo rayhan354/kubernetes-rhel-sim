48b9c30e00f6856210440d3a52cfc1a6662fcb68
rayhan354 <rayhan354@users.noreply.github.com>
2025-08-05 11:10:52 +0700

    Update setup-control-plane.sh

--- setup-control-plane.sh
+++ setup-control-plane.sh
@@ -88,4 +88,5 @@ echo ""
 echo "To add worker nodes to the cluster, run the following command on each worker:"
 echo "-------------------------------------------------------------------------"
 kubeadm token create --print-join-command
+echo "Use 'kubectl get nodes"
 echo "-------------------------------------------------------------------------"
