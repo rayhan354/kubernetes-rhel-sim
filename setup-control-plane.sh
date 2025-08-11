fdb402d1f11c21bbe0626543f7396abb2ab2cc2f
rayhan354 <rayhan354@users.noreply.github.com>
2025-08-11 16:10:24 +0700

    Update setup-control-plane.sh

--- setup-control-plane.sh
+++ setup-control-plane.sh
@@ -10,55 +10,6 @@ echo "### [PHASE 1] Starting Kubernetes Control-Plane Setup ###"
 
 # --- [PREREQUISITES] ---
 echo "--> [1/4] Running prerequisite steps..."
-# --- Source OS information ---
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-else
-    echo "Cannot determine OS version. /etc/os-release not found."
-    exit 1
-fi
-
-# Check for RHEL
-if [[ $ID == "rhel" ]]; then
-    if [[ $VERSION_ID == 9* ]]; then
-        echo "Enabling CodeReady Builder for RHEL 9..."
-        subscription-manager repos --enable codeready-builder-for-rhel-9-x86_64-rpms
-    elif [[ $VERSION_ID == 8* ]]; then
-        echo "Enabling CodeReady Builder for RHEL 8..."
-        subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
-    else
-        echo "Unsupported RHEL version: $VERSION_ID"
-        exit 1
-    fi
-    
-# Check for CentOS
-elif [[ $ID == "centos" ]]; then
-    if [[ $VERSION_ID == 9* ]]; then
-        echo "Enabling CRB repository for CentOS Stream 9..."
-        dnf config-manager --set-enabled crb
-    elif [[ $VERSION_ID == 8* ]]; then
-        # For CentOS 8, the repo was called "PowerTools"
-        echo "Enabling PowerTools repository for CentOS Stream 8..."
-        dnf config-manager --set-enabled powertools
-    else
-        echo "Unsupported CentOS version: $VERSION_ID"
-        exit 1
-    fi
-# Handle other operating systems
-else
-    echo "This script is intended for RHEL or CentOS only. Aborting."
-    exit 1
-fi
-
-# Install the docker
-dnf install –y dnf-plugins-core 
-dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-dnf config-manager --set-enabled crb || 
-systemctl enable --now docker
-
-docker version
-
 # Disable swap
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
@@ -101,14 +52,20 @@ systemctl enable containerd > /dev/null 2>&1
 
 # --- [KUBERNETES PACKAGES] ---
 echo "--> [3/4] Installing kubeadm, kubelet, and kubectl..."
-# Add Kubernetes repo
+
+# Define the latest Kubernetes version
+K8S_VERSION="v1.30" # <-- CHANGE: Updated to the latest stable version
+
+# Add Kubernetes repo using the new community-owned repository
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+# <-- CHANGE: Updated repository URL to point to the latest stable version channel
+baseurl=https://pkgs.k8s.io/core:/stable:/${K8S_VERSION}/rpm/
 enabled=1
 gpgcheck=1
-gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+# <-- CHANGE: Updated GPG key URL to match the new repository
+gpgkey=https://pkgs.k8s.io/core:/stable:/${K8S_VERSION}/rpm/repodata/repomd.xml.key
 exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
 
@@ -118,10 +75,6 @@ systemctl enable --now kubelet > /dev/null 2>&1
 
 # --- [INITIALIZE CLUSTER] ---
 echo "--> [4/4] Initializing Kubernetes cluster with kubeadm..."
-# --- [NETWORK CNI] ---
-echo "--> Installing Calico network CNI..."
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/calico.yaml
-
 # Initialize control plane
 kubeadm init --pod-network-cidr=192.168.0.0/16
 
@@ -131,11 +84,15 @@ mkdir -p "/home/$(logname)/.kube"
 cp -i /etc/kubernetes/admin.conf "/home/$(logname)/.kube/config"
 chown "$(id -u $(logname)):$(id -g $(logname))" "/home/$(logname)/.kube/config"
 
+# --- [NETWORK CNI] ---
+echo "--> Installing Calico network CNI..."
+# <-- CHANGE: Updated Calico manifest URL to the latest version recommended by Project Calico
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.0/manifests/calico.yaml
+
 echo ""
 echo "### [SUCCESS] Your Kubernetes control-plane has been initialized! ###"
 echo ""
 echo "To add worker nodes to the cluster, run the following command on each worker:"
 echo "-------------------------------------------------------------------------"
 kubeadm token create --print-join-command
-echo "Use 'kubectl get nodes' to get the nodes list."
 echo "-------------------------------------------------------------------------"
