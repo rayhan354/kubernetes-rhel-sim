284581437d31dd2100f940abb0f190b93207ced5
rayhan354 <rayhan354@users.noreply.github.com>
2025-08-15 11:16:36 +0700

    Update setup-worker-node.sh

--- setup-worker-node.sh
+++ setup-worker-node.sh
@@ -52,7 +52,7 @@ else
 fi
 
 # Install the docker
-dnf install –y dnf-plugins-core 
+dnf install -y dnf-plugins-core 
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 dnf config-manager --set-enabled crb || 
